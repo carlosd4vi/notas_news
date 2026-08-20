1f8b676e050770b5581d3c912f8b1088b62cb9fc
Carlos Davi <carlosdavi1314@gmail.com>
2026-08-20 15:57:01 -0300

    Encerrar sessão

--- session/sair.php
+++ session/sair.php
@@ -1,14 +1,10 @@
 <?php
-// Inicia a sessão
 session_start();
 
-// Remove todas as variáveis da sessão
 $_SESSION = array();
 
-// Destrói a sessão
 session_destroy();
 
-// Redireciona para a página de login
 header("location: login.php");
 exit;
 ?>
